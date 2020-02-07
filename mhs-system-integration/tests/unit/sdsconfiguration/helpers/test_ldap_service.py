0d7bf99b7a024a5c0bc336e10e6044c68b5dff1b
bfisher <fishey2b@gmail.com>
2020-02-07 15:10:23 +0000

    [PRMT-940] Restructured project to enable source imports in test

--- mhs-system-integration/tests/unit/sdsconfiguration/helpers/test_ldap_service.py
+++ mhs-system-integration/tests/unit/sdsconfiguration/helpers/test_ldap_service.py
@@ -1,4 +1,5 @@
 import unittest
+from sdsconfiguration.helpers.ldap_service import LdapService
 
 
 class TestLdapService(unittest.TestCase):
@@ -8,6 +9,10 @@ class TestLdapService(unittest.TestCase):
 
     def test_should_be_true(self):
         self.assertTrue(True)
+
+    def test_should_take_ldap_endpoint(self):
+        ldap_service = LdapService()
+        self.assertIsNotNone(ldap_service)
 
 
 if __name__ == '__main__':
